7952aa401076735682d3067cfe99b55dc5571126
Aifedayo <akeemifedayolag@gmail.com>
2024-02-08 10:00:00 +0100

    Duplicates within range

---
+++ Arrays and Strings/Sliding Window/duplicates_within_range.py
@@ -0,0 +1,7 @@
+"""
+Given an integer array arr and an integer k, 
+determine if there are any duplicate values 
+within k indices of each other. In other words, 
+check if the same value exists at two different 
+indices that are at most k positions apart.
+"""
