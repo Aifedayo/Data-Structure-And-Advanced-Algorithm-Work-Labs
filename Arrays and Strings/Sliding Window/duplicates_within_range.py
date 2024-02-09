a41abf83061d2cfebb3ec60cde8a7edbe74bd779
Aifedayo <akeemifedayolag@gmail.com>
2024-02-09 10:00:00 +0100

    Duplicates within range

--- Arrays and Strings/Sliding Window/duplicates_within_range.py
+++ Arrays and Strings/Sliding Window/duplicates_within_range.py
@@ -5,3 +5,15 @@ within k indices of each other. In other words,
 check if the same value exists at two different 
 indices that are at most k positions apart.
 """
+
+def duplicate_in_range(arr, k):
+    unique_set = set()
+
+    for right in range(len(arr)):
+        if arr[right] in unique_set:
+            return True
+        unique_set.add(arr[right])
+        
+    return False
+
+print(duplicate_in_range(arr=[1,2,3,1,4,5],k=3))
