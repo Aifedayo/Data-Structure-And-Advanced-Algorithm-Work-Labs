a18648b100bdfcea4f7bc1a03ea510b13fb49ff6
Aifedayo <akeemifedayolag@gmail.com>
2024-04-08 10:00:00 +0100

    Practice

--- Arrays and Strings/pract.py
+++ Arrays and Strings/pract.py
@@ -1,12 +1,6 @@
 def find_majority_element(arr):
 
-    hashfreq = {}
-
-    for num in arr:
-        hashfreq[num] = hashfreq.get(num, 0) + 1
-
-    maj_element = max(hashfreq, key=hashfreq.get)
-    return maj_element
+    return max(set(arr), key=arr.count)
         
 
 print(find_majority_element([1,1,2,4,5,5,5,5]))
