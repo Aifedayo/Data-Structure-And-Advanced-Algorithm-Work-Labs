4bc2d5a2c0b5382c63a69b05e6b5edccd36807e1
Aifedayo <akeemifedayolag@gmail.com>
2024-04-09 10:00:00 +0100

    Practice

--- Arrays and Strings/pract.py
+++ Arrays and Strings/pract.py
@@ -1,5 +1,16 @@
-def delete_at_pos(arr, pos):
-    return arr[:pos] + arr[pos+1:]
+def first_occurence(arr, target):
+    left, right = 0, len(arr)-1
+
+    while left <= right:
+        mid = (left + right)//2
+        if arr[mid] == target and (mid == 0 or arr[mid-1] != target):
+            return mid
+        elif arr[mid] >= target and (mid == 0 or arr[mid-1] == target):
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
         
 
-print(delete_at_pos([1,1,2,4,5,5,5,5], 0))
+
+print(first_occurence(arr=[2, 4, 4, 4, 6, 6, 8, 10], target=4))
