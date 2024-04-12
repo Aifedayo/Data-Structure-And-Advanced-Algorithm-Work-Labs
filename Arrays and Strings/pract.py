0b2f8712e980cc9b8ecd0e1206af950f3658dc0c
Aifedayo <akeemifedayolag@gmail.com>
2024-04-12 10:00:00 +0100

    Practice

--- Arrays and Strings/pract.py
+++ Arrays and Strings/pract.py
@@ -1,15 +1,21 @@
-def peak_element(arr):
-    left, right = 0, len(arr)-1
+def search_rotated(nums, target):
+    left, right = 0, len(nums)-1
 
     while left <= right:
         mid = (left + right) // 2
-        if arr[mid] > arr[mid-1] and arr[mid] > arr[mid+1] and mid > 0:
+        if nums[mid] == target:
             return mid
-        if arr[mid] < arr[mid-1] and mid > 0:
-            right = mid - 1
+        if nums[mid] >= nums[left]:
+            if target <= nums[mid] and target >= nums[left]:
+                right = mid - 1
+            else:
+                left = mid + 1
         else:
-            left = mid + 1
-    return -1 
-        
+            if target <= nums[mid] and target <= nums[left]:
+                right = mid - 1
+            else:
+                left = mid + 1
 
-print(peak_element([1, 2, 1, 3, 5, 6, 4]))
+    return -1
+
+print(search_rotated(nums=[4,5,6,7,0,1,2], target=2))
