86dfd94e1bf158ec2301c5e005cf72fa4a5a1d2f
Aifedayo <akeemifedayolag@gmail.com>
2024-04-12 10:00:00 +0100

    Practice

--- Arrays and Strings/pract.py
+++ Arrays and Strings/pract.py
@@ -1,41 +1,15 @@
-def find_first_and_last(arr, target):
-    def first(arr, target):
-        left, right = 0, len(arr)-1
-        position = -1
+def peak_element(arr):
+    left, right = 0, len(arr)-1
 
-        while left <= right:
-            mid = (left+right)//2
-            if arr[mid] == target:
-                position = mid
-                right = mid - 1
-            elif arr[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-        return position
-    
-    def last(arr, target):
-        left, right = 0, len(arr)-1
-        position = -1
-
-        while left <= right:
-            mid = (left + right) // 2
-            if arr[mid] == target:
-                position = mid
-                left = mid + 1
-            elif arr[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
-
-        return position
-    
-    first = first(arr, target)
-    last = last(arr, target)
-    return [first, last]
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > arr[mid-1] and arr[mid] > arr[mid+1] and mid > 0:
+            return mid
+        if arr[mid] < arr[mid-1] and mid > 0:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1 
         
 
-arr = [5, 7, 8, 8, 8, 8, 10, 11, 34]
-target = 11
-
-print(find_first_and_last(arr, target))
+print(peak_element([1, 2, 1, 3, 5, 6, 4]))
