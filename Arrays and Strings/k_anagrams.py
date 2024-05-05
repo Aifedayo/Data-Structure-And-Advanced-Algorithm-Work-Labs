356cc0b8cc17b2938967410a96abdfef224d7725
Aifedayo <akeemifedayolag@gmail.com>
2024-05-05 10:00:00 +0100

    k-anagram

--- Arrays and Strings/k_anagrams.py
+++ Arrays and Strings/k_anagrams.py
@@ -9,3 +9,6 @@ def k_anagrams(s1, s2, k):
         freq_s1[char] = freq_s1.get(char, 0) + 1
     for char in s2:
         freq_s2[char] = freq_s2.get(char, 0) + 1
+
+    mismatch_count = 0
+    for char in freq_s1
