12cd1e076fe35faf34f8574e3b9d87ddd51816a1
Aifedayo <akeemifedayolag@gmail.com>
2024-05-16 10:00:00 +0100

    k-anagram

--- Arrays and Strings/k_anagrams.py
+++ Arrays and Strings/k_anagrams.py
@@ -10,15 +10,21 @@ def k_anagrams(s1, s2, k):
     for char in s2:
         freq_s2[char] = freq_s2.get(char, 0) + 1
 
+    print(freq_s1)
+    print(freq_s2)
+
     mismatch_count = 0
     for char in freq_s1:
         if char in freq_s2:
             mismatch_count += abs(freq_s1[char] - freq_s2[char])
         else:
             mismatch_count += freq_s1[char]
+        print(mismatch_count)
 
     return mismatch_count // 2 <= k
+    # The final division by 2 in the return statement accounts 
+    # for each pair of mismatched characters
 
 
 print(k_anagrams("anagram", "mangaar", 2))
-print(k_anagrams("anagram", "grammar", 3))
+print(k_anagrams("anagram", "grammar", 1))
