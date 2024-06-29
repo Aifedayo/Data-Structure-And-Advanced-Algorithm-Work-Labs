b06e77de524f7fa0135676d1b0768cacd16cad7b
Aifedayo <akeemifedayolag@gmail.com>
2024-06-29 10:00:00 +0100

    Rearrange folders

--- Arrays and Strings/HashSets/first_non_repeating_chars.py
+++ Arrays and Strings/HashSets/first_non_repeating_chars.py
@@ -10,6 +10,10 @@ def first_non_repeating(s):
     for char in s:
         if char in seen_multiple:
             continue
+        if char in seen_once:
+            seen_multiple.add(char)
+        else:
+            seen_once.append(char)
         
 
     for key, value in hashstring.items():
