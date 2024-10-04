c218b31acb72d2507e8ea05b9571d6a1ad534108
Aifedayo <akeemifedayolag@gmail.com>
2024-10-04 19:22:23 +0100

    Rename queue

---
+++ printer-job-queue.py
@@ -0,0 +1,13 @@
+from collections import deque
+from queue_dsa import Queue
+
+class PrinterQueue:
+    """
+    Task: Implement a Queue to Simulate a Printer's Job Queue
+    Objective: Simulate how jobs are handled in a printer queue. 
+    The queue should process jobs in the order they are added.
+    """
+    def __init__(self):
+        self.queue = deque()
+
+    def 
