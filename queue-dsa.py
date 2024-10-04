2e4edc5dbcd269e6fc0f83cf7b06dd192a7e527b
Aifedayo <akeemifedayolag@gmail.com>
2024-10-04 17:14:35 +0100

    Implement a queue with python

---
+++ queue-dsa.py
@@ -0,0 +1,45 @@
+"""
+Task: Implement a Basic Queue
+Objective: Create a simple queue data structure using Python. A queue follows the First In, First Out (FIFO) principle, 
+meaning the first element added is the first one to be removed.
+"""
+
+# Implement a Queue : FIFO
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def enqueue(self, item):
+        return self.queue.appendleft(item)
+    
+    def dequeue(self):
+        try:
+            if not self.is_empty():
+                return self.queue.pop()
+        except Exception as e:
+            return str(e)
+
+    def peek(self):
+        try:
+            if not self.is_empty():
+                return self.queue[-1]
+        except Exception as e:
+            return (str(e))
+
+    def size(self):
+        return len(self.queue)
+
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+print(queue.peek())  # Output: 1
+print(queue.dequeue())  # Output: 1
+print(queue.is_empty())  # Output: False
+print(queue.size())  # Output: 2
