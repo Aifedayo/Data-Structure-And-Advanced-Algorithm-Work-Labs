81a91dad47cc0d378bad72bc79e69f0fd0c179fc
Aifedayo <akeemifedayolag@gmail.com>
2024-10-07 06:53:45 +0100

    space and time complexity for nested for loops

--- quadratic_time.py
+++ quadratic_time.py
@@ -2,7 +2,10 @@ def print_pairs(arr):
     for i in range(len(arr)):
         for j in range(len(arr)):
             print(arr[i], arr[j])
- # Time grows quadratically as input size increases
+
+# Time complexity: O(n^2) grows quadratically as input size increases
+# Space Complexity: 0(1) space used is constant
+
 
 arr = [1,2,3]
 print_pairs(arr)
