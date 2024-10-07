ee173cddee68fcdf70dd3e2122b4d7a8d802242a
Aifedayo <akeemifedayolag@gmail.com>
2024-10-07 06:50:04 +0100

    find duplicates cmplexity analysis

---
+++ find_duplicates.py
@@ -0,0 +1,13 @@
+def find_duplicates(arr):
+    duplicates = []
+    seen = set()
+    for num in arr:
+        if num in seen:
+            duplicates.append(num)
+        else:
+            seen.add(num)
+    return duplicates
+
+# Time complexity: 0(n) Because we loop through once, algo run time gets bigger per input size
+
+# Space complexity: 0(n^2): The space used doubles with each input size
