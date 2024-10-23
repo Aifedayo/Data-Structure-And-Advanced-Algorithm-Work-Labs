6f813c84d4ffa5c1d8e363f4f4cd71d823dcded0
Aifedayo <akeemifedayolag@gmail.com>
2024-10-23 11:26:29 +0100

    longest substring sliding window approach

---
+++ Arrays and Strings/longest_substring.py
@@ -0,0 +1,16 @@
+def longest_subtring(s):
+    char_set = set()
+    left, right = 0, 0
+    max_length = 0
+
+    while right < len(s):
+        if s[right] in char_set:
+            char_set = set()
+        char_set.add(s[right])
+        right += 1
+        max_length = max(max_length, len(char_set))
+
+    return max_length
+
+
+print(longest_subtring("zainb"))
