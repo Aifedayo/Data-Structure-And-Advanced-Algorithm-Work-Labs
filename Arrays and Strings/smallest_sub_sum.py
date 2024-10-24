83cbf412fe95bd8a866e5705d861851e6a15e8ac
Aifedayo <akeemifedayolag@gmail.com>
2024-10-24 11:09:41 +0100

    smallest sub array sliding window

---
+++ Arrays and Strings/smallest_sub_sum.py
@@ -0,0 +1,17 @@
+def smallest_sub_array(arr, S):
+    window_sum =0
+    min_length = float('inf')
+    left = 0
+
+    for right in range(len(arr)):
+        window_sum += arr[right]
+
+        while window_sum >= S:
+            min_length = min(min_length, right - left + 1)
+            window_sum -= arr[left]
+            left += 1
+
+    return min_length if min_length != float('inf') else 0
+
+
+print(smallest_sub_array(arr=[2,1,5,2,3,2], S=5))
