b2a225258c0284ccded4161ae5aa0de6e9cf866b
Aifedayo <akeemifedayolag@gmail.com>
2024-10-24 21:01:28 +0100

    optimized code for sliding window

--- Arrays and Strings/longest_distinct_substring.py
+++ Arrays and Strings/longest_distinct_substring.py
@@ -1,17 +1,20 @@
 def longest_distinct_substring(s, k):
-    curr_window = ''
+    char_count = {}
     max_length = 0
     left = 0
 
     for right in range(len(s)):
-        curr_window += s[right]
+        char_count[s[right]] = char_count.get(s[right], 0) + 1
+        print(char_count)
 
-        while len(set(curr_window)) > k:
-            curr_window = s[left+1:right+1]
+        while len(char_count) > k:
+            char_count[s[left]] -= 1
+            if char_count[s[left]] == 0:
+                del char_count[s[left]]
             left += 1
 
         max_length = max(max_length, right - left+1)
     return max_length
 
 
-print(longest_distinct_substring(s='akeeem', k=2))
+print(longest_distinct_substring(s='akeeemm', k=2))
