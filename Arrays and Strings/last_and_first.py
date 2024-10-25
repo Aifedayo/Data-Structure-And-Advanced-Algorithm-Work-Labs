c33e85e27b4499e89f3fe5178410339ec4305a09
Aifedayo <akeemifedayolag@gmail.com>
2024-10-25 09:38:19 +0100

    find the first and last in a sorted array

--- Arrays and Strings/last_and_first.py
+++ Arrays and Strings/last_and_first.py
@@ -1,25 +1,40 @@
-def binary_search(arr, target):
-    left, right = 0, len(arr)-1
-    position = [-1, -1]
+def find_first_and_last(arr, target):
+    def binary_search_first(arr, target):
+        left, right = 0, len(arr)-1
+        position = -1
 
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            if arr[mid-1] == target:
-                position[0] = mid-1
-                position[1] = mid
-            elif arr[mid+1] == target:
-                position[0] = mid
-                position[-1] = mid+1
-            return position
-        elif arr[mid] > target:
-            right = mid - 1
-        else:
-            left = mid + 1
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                position = mid
+                right = mid - 1
 
-    return position
+            elif arr[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return position
+    
+    def binary_search_last(arr, target):
+        left, right = 0, len(arr)-1
+        position = -1
 
-arr = [5, 7, 7, 7, 8, 8, 10, 11, 34]
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                position = mid
+                left = mid + 1
+            elif arr[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return position
+    
+    first = binary_search_first(arr, target)
+    last = binary_search_last(arr, target)
+    return [first, last]
+
+arr = [5, 7, 8, 8, 8, 8, 10, 11, 34]
 target = 8
 
-print(binary_search(arr, target))
+print(find_first_and_last(arr, target))
