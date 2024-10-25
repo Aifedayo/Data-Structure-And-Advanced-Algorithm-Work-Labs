8f82a4132720265382aae20c197edd10b77dbd7c
Aifedayo <akeemifedayolag@gmail.com>
2024-10-25 11:39:37 +0100

    square root using binary search

---
+++ Arrays and Strings/square_root_binary.py
@@ -0,0 +1,23 @@
+def square_root(x):
+    if x < 2:
+        return x
+    
+    left, right = 0, x
+
+    while left <= right:
+        mid = (right + left)//2
+        print('mid: ', mid)
+        mid_squared = mid * mid
+
+        if mid_squared == x:
+            return mid
+        
+        elif mid_squared < x:
+            left = mid + 1
+            result = mid
+        else:
+            right = mid - 1
+
+    return result
+
+print(square_root(8))
