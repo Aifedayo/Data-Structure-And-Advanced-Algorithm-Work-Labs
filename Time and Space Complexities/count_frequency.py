603a3278099c0ebba1dc7634314123327b451db6
Aifedayo <akeemifedayolag@gmail.com>
2024-10-26 08:37:34 +0100

    Refactor frequency counter

--- Time and Space Complexities/count_frequency.py
+++ Time and Space Complexities/count_frequency.py
@@ -1,10 +1,7 @@
 def count_frequency(arr):
     freq = {}
     for num in arr:
-        if num in freq:
-            freq[num] += 1
-        else:
-            freq[num] = 1
+        freq[num] = freq.get(num, 0) + 1
 
     return freq
 
