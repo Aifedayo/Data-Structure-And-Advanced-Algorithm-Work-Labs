99b887074fc3981117e97abd97fbc1519d005b60
Aifedayo <akeemifedayolag@gmail.com>
2024-10-26 08:50:21 +0100

    Anagram with hashmap

--- Arrays and Strings/pract.py
+++ Arrays and Strings/pract.py
@@ -1,16 +1,20 @@
-def smallest_subar(arr, k):
-    curr_sum = 0
-    max_sum = 0
+def anagram(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    
+    # using hashmap
+    freqmap1 = {}
+    freqmap2 = {}
 
-    for i in range(k):
-        curr_sum += arr[i]
+    for i in range(len(s1)):
+        freqmap1[s1[i]] = freqmap1.get(s1[i], 0) + 1
+        freqmap2[s2[i]] = freqmap2.get(s2[i], 0) + 1
 
-    max_sum = curr_sum
+    for i in freqmap1:
+        if freqmap1[i] != freqmap2.get(i, 0):
+            return False
+    
+    return True
+    
 
-    for i in range(k, len(arr)):
-        curr_sum = curr_sum - arr[i-k] + arr[i]
-        max_sum = max(max_sum, curr_sum)
-
-    return max_sum
-
-print(smallest_subar(arr=[2,1,5,1,3,2], k=3))
+print(anagram('silent', 'listen'))
