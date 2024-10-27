08f2a12837112f50e4c60138d065dfc5435b4fe8
Aifedayo <akeemifedayolag@gmail.com>
2024-10-27 07:17:26 +0100

    singlylist practice

--- Arrays and Strings/pract.py
+++ Arrays and Strings/pract.py
@@ -1,9 +1,49 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, next=None):
         self.data = data
         self.next = None
 
 
-class LinkedList:
+class SinglyLinkedList:
     def __init__(self):
         self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def insert_at_end(self, data):
+        node = Node(data)
+
+        if self.head is None:
+            self.head = Node
+            return
+        
+        curr_head = self.head
+        while curr_head.next:
+            curr_head = curr_head.next
+
+        curr_head.next = node
+
+    def search(self, data):
+        if self.head is None:
+            return f'Empty List!'
+        
+        else:
+            curr = self.head
+            while curr.next:
+                if curr.data == data:
+                    return True
+                curr = curr.next
+        return f'Not found'
+
+
+ll = SinglyLinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.traverse()  # Expected output: 1 -> 2 -> 3 -> None
+ll.delete(2)
+ll.traverse()
+        
+
