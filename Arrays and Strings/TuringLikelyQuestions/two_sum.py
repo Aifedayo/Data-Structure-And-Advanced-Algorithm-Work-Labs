de2bdadb4217c56edcadbeaabdccef9f67b208cc
Aifedayo <akeemifedayolag@gmail.com>
2024-10-27 19:24:34 +0100

    two sum

---
+++ Arrays and Strings/TuringLikelyQuestions/two_sum.py
@@ -0,0 +1,15 @@
+"""
+Problem: Given an array of integers, return indices of the two numbers that add up to a 
+specific target.
+"""
+
+def two_sum(arr, target):
+    hashsum = {}
+    for idx, num in enumerate(arr):
+        diff = target - num
+        if diff in hashsum:
+            return [hashsum[diff], idx]
+        else:
+            hashsum[num] = idx
+
+    return []
