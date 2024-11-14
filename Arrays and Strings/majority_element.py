7dbc8d8b4d1dd8f7494b564882af3af841d9df4f
Aifedayo <akeemifedayolag@gmail.com>
2024-11-14 07:47:11 +0100

    most frequenct element using arr.count as the key

--- Arrays and Strings/majority_element.py
+++ Arrays and Strings/majority_element.py
@@ -1,12 +1,15 @@
 def majority_element(arr):
-    freq_map = {}
-    for num in arr:
-        freq_map[num] = freq_map.get(num, 0) + 1
+    # freq_map = {}
+    # for num in arr:
+    #     freq_map[num] = freq_map.get(num, 0) + 1
     
-    for key, count in freq_map.items():
-        if count > len(arr) // 2:
-            return key
-    return None
+    # for key, count in freq_map.items():
+    #     if count > len(arr) // 2:
+    #         return key
+    # return None
+
+
+    return max(arr, key=arr.count)
     
 
 arr = [2, 2, 1, 1, 2, 2]
