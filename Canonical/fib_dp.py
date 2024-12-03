40122c6b5dc3871f223b970afd961a567a8e0747
Aifedayo <akeemifedayolag@gmail.com>
2024-12-03 10:36:37 +0100

    binary sort

--- Canonical/fib_dp.py
+++ Canonical/fib_dp.py
@@ -11,3 +11,16 @@ def fib_dp(n):
     return table[n]
 
 print(fib_dp(10))
+
+def binary_sort(arr, target):
+    left, right = 0, len(arr)-1
+
+    while left <= right:
+        mid = (left+ right) // 2
+        if arr[mid] == target:
+            return mid
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
